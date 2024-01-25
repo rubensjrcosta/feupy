9a6e0cbeaa435dc716ff75c33cd9bf2936202731
rubensjrcosta <rubensjrcosta@gmail.com>
2024-01-25 17:20:32 -0300

    Return analysis module

--- feupy/analysis/core.py
+++ feupy/analysis/core.py
@@ -16,11 +16,15 @@ import pandas as pd
 from feupy.roi import ROI
 from feupy.target import Target
 from feupy.utils.string_handling import name_to_txt
+from feupy.utils.datasets import cut_energy_table_fp
+
 from feupy.scripts import gammapy_catalogs 
 
 from feupy.catalog.pulsar.atnf import SourceCatalogATNF
 from feupy.catalog.lhaaso import SourceCatalogPublishNatureLHAASO
 from feupy.catalog.hawc import SourceCatalogExtraHAWC
+
+from feupy.analysis import CounterpartsAnalysisConfig
 
 from astropy import units as u
 from astropy.coordinates import SkyCoord
@@ -41,72 +45,13 @@ from pathlib import Path
 # In[4]:
 
 
-__all__ = ["AnalysisConfig", "Analysis"]
-
-
-# In[5]:
-
-
-# How to create a class:
-class AnalysisConfig:
-   # ADD others parameters
-   
-    # color="red" # The color of the flux ponts
-    all=[]
-    @u.quantity_input(pos_ra=u.deg, pos_dec=u.deg, radius=u.deg, e_ref_min=u.eV, e_ref_max=u.eV)
-    def __init__(self, target_name: str, pos_ra, pos_dec, radius, e_ref_min=None, e_ref_max=None, catalogs_roi=None):
-       # Run validations to the received arguments
-        assert  0 <= pos_ra.value <= 360, f"Right Ascension {pos_ra} is not in the range: (0,360) deg!"
-        assert -90 <= pos_dec.value <= 90, f"Declination {pos_dec} is not in the range: (-90,90) deg!"
-
-        # Assign to self object
-        self.__target_name = target_name
-        self.position = SkyCoord(pos_ra, pos_dec) 
-        self.radius = radius
-        if e_ref_min is not None:
-            self.e_ref_min = Quantity(e_ref_min, "TeV")
-        else: self.e_ref_min = e_ref_min
-        if e_ref_max is not None:
-            self.e_ref_max = Quantity(e_ref_max, "TeV")
-        else: self.e_ref_max = e_ref_max
-        self.energy_range = [self.e_ref_min, self.e_ref_max]
-        self.target = Target(self.__target_name, self.position.ra, self.position.dec)
-        self.roi = ROI(self.__target_name, self.position.ra, self.position.dec, self.radius)
-        
-        # Actions to execute
-        AnalysisConfig.all.append(self)
-    
-    @property
-    # Property Decorator=Read-Only Attribute
-    def info(self):
-        info={}
-        info["target_name"] = self.target_name
-        info["position"] = self.position
-        info["radius"] = self.radius
-        info["energy_range"] = self.energy_range
-        return info    
-    
-    @property
-    def target_name(self):
-        return self.__target_name
-
-    def __repr__(self):
-        ss = f"{self.__class__.__name__}("
-        ss += f"target_name={self.__target_name}, "
-        ss += "pos_ra=Quantity('{:.2f}'), ".format(self.position.ra).replace(' ', '')
-        ss += "pos_dec=Quantity('{:.2f}'), ".format(self.position.dec).replace(' ', '')
-        ss += "radius=Quantity('{:.2f}'), ".format(self.radius).replace(' ', '')
-        if self.e_ref_min is None: ss += "e_ref_min=None, "
-        else: ss += "e_ref_min=Quantity('{}'), ".format(energy_unit_format(self.e_ref_min).replace(' ', ''))
-        if self.e_ref_max is None: ss += "e_ref_max=None)"
-        else: ss += "e_ref_max=Quantity('{}'))".format(energy_unit_format(self.e_ref_max).replace(' ', ''))
-        return ss 
+__all__ = ["CounterpartsAnalysis"]
 
 
 # In[3]:
 
 
-class Analysis:
+class CounterpartsAnalysis:
     """Config-driven high level analysis interface.
 
     It is initialized by default with a set of configuration parameters and values declared in
@@ -116,8 +61,8 @@ class Analysis:
 
     Parameters
     ----------
-    config : dict or `~gammapy.analysis.counterparts.AnalysisConfig`
-        Configuration options following `AnalysisConfig` schema.
+    config : dict or `~gammapy.analysis.counterparts.CounterpartsAnalysisConfig`
+        Configuration options following `CounterpartsAnalysisConfig` schema.
     """
 
     def __init__(self, config):
@@ -133,15 +78,15 @@ class Analysis:
         
     @property
     def config(self):
-        """Analysis configuration as an `~feupy.analysis.counterparts.AnalysisConfig` object."""
+        """Analysis configuration as an `~feupy.analysis.CounterpartsAnalysisConfig` object."""
         return self._config
 
     @config.setter
     def config(self, value):
-        if isinstance(value, AnalysisConfig):
+        if isinstance(value, CounterpartsAnalysisConfig):
             self._config = value
         else:
-            raise TypeError("config must be AnalysisConfig.")
+            raise TypeError("config must be CounterpartsAnalysisConfig")
             
     def run(self):
         self._get_catalogs()
@@ -221,7 +166,7 @@ class Analysis:
                         )
 
                         if any([self.config.e_ref_min !=  None, self.config.e_ref_max !=  None]):
-                            dataset = self._cut_energy_flux_points(dataset)
+                            dataset = cut_energy_table_fp(dataset, self.config.e_ref_min, self.config.e_ref_max) 
 
                         n_flux_points += 1
                         models.append(model)  # Add the model to models()
@@ -243,38 +188,7 @@ class Analysis:
         print(f"Total number of gammapy sources: {n_sources-len(pulsars)}")
         print(f"Total number of flux points tables: {n_flux_points}")
         print(f"Total number of pulsars: {len(pulsars)}")
-        
-    def _cut_energy_flux_points(self, dataset):
-        _datasets = Datasets()
-        e_ref_min = self.config.e_ref_min
-        e_ref_max = self.config.e_ref_max
-
-        flux_points = dataset.data
-        models = dataset.models[0]      
-        ds_name = dataset.name
-
-        if e_ref_min != None:
-            mask_energy = np.zeros(len(flux_points.to_table()), dtype=bool)
-
-            for m, e_ref in enumerate(flux_points.energy_ref):
-                if e_ref >= e_ref_min:
-                    mask_energy[m] = True
-
-            flux_points_mask = flux_points.to_table()[mask_energy]
-            flux_points = FluxPoints.from_table(flux_points_mask)
-
-        if e_ref_max != None:
-            mask_energy = np.zeros(len(flux_points.to_table()), dtype=bool)
-
-            for m, e_ref in enumerate(flux_points.energy_ref):
-                if e_ref <= e_ref_max:
-                    mask_energy[m] = True
-
-            flux_points_mask = flux_points.to_table()[mask_energy]
-            flux_points = FluxPoints.from_table(flux_points_mask)     
-
-        return FluxPointsDataset(models=models, data=flux_points, name=ds_name)
-     
+             
     def _get_dict_roi(self):
         _dict_roi = {}
 
@@ -384,7 +298,7 @@ class Analysis:
 
 
 def test_analysis_confg():
-    return AnalysisConfig(
+    return CounterpartsAnalysisConfig(
         "LHAASO J1825-1326", 
         276.45* u.Unit('deg'), 
         -13.45* u.Unit('deg'),
--- feupy/counterparts/__init__.py
+++
@@ -1,7 +0,0 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-"""Counterparts."""
-from .counterparts import AnalysisConfig, Analysis
-__all__ = [
-    "Analysis",
-    "AnalysisConfig",
-]
