237bc0c60279a9b804ff3d608142b0f7f8a46b28
Rubens Costa Jr. <rubensjrcosta@gmail.com>
2026-03-15 17:03:26 -0300

    Create __init__.py

---
+++ feupy/visualization/utils/__init__.py
@@ -0,0 +1,12 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+Visualization subpackage for FeuPy.
+
+This subpackage provides labels, styles, and plotting helpers.
+"""
+
+__all__ = [
+    "labels",
+]
+
+from . import labels
