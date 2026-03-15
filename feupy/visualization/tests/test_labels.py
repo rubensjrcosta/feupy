27c5b4e905d9a72e86a7fd14dc0976572672ed65
Rubens Costa Jr. <rubensjrcosta@gmail.com>
2026-03-15 17:03:22 -0300

    Create test_labels.py

---
+++ feupy/visualization/tests/test_labels.py
@@ -0,0 +1,19 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+from feupy.visualization import labels
+
+def test_yaxis_labels_keys():
+    assert "e2dnde" in labels.DEFAULT_YAXIS_LABEL
+    assert "dnde" in labels.DEFAULT_YAXIS_LABEL
+
+def test_yaxis_labels_content():
+    assert "E^{2}" in labels.DEFAULT_YAXIS_LABEL["e2dnde"]
+    assert "\\Phi(E)" in labels.DEFAULT_YAXIS_LABEL["dnde"]
+
+def test_xaxis_labels_keys():
+    assert "erg" in labels.DEFAULT_XAXIS_LABEL
+    assert "TeV" in labels.DEFAULT_XAXIS_LABEL
+
+def test_xaxis_labels_content():
+    assert "Energy" in labels.DEFAULT_XAXIS_LABEL["erg"]
+    assert "Energy" in labels.DEFAULT_XAXIS_LABEL["TeV"]
