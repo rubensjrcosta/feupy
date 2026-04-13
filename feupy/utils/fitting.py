55b80808d6cb6db5c42e9a440f2bdf5c4e65de66
Rubens Costa Jr. <rubensjrcosta@gmail.com>
2026-04-13 11:22:02 -0300

    Add new utils/fitting.py and related test suites

---
+++ feupy/utils/fitting.py
@@ -0,0 +1,25 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+Spectral fitting utilities.
+"""
+
+from gammapy.modeling import Fit
+from gammapy.datasets import Datasets, FluxPointsDataset
+from gammapy.estimators import FluxPoints
+from gammapy.modeling.models import SkyModel, Models
+
+__all__ = ["fit_spectral_model_to_flux_points"]
+
+
+def fit_spectral_model_to_flux_points(flux_points_table, spectral_model):
+    """Fit spectral model to flux points."""
+
+    flux_points = FluxPoints.from_table(flux_points_table)
+    datasets = Datasets(FluxPointsDataset(data=flux_points))
+
+    model = SkyModel(spectral_model=spectral_model)
+    datasets.models = model
+
+    Fit().run(datasets=datasets)
+
+    return model.spectral_model
