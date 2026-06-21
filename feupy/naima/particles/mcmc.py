143326caf7fb8c4a0396bcf71c071350bd1d4aa6
Rubens Costa Jr. <rubensjrcosta@gmail.com>
2026-06-21 19:45:37 -0300

    update mcmc

--- feupy/naima/particles/mcmc.py
+++ feupy/naima/particles/mcmc.py
@@ -11,13 +11,13 @@ def _get_mcmc_median(table, label):
     return table[table["label"] == label]["median"][0]
 
 
-def _get_amplitude_from_mcmc(table, label="log10_norm", unit=u.eV):
-    """Return amplitude from log10_norm parameter."""
+def _get_amplitude_from_mcmc(table, label="log10(norm)", unit=u.eV):
+    """Return amplitude from log10(norm) parameter."""
     return 10 ** _get_mcmc_median(table, label) / unit
 
 
 def _get_energy_from_log10(table, label, unit=u.TeV):
-    """Return energy from log10_E_unit."""
+    """Return energy from log10(energy)."""
     return 10 ** _get_mcmc_median(table, label) * unit
 
 
@@ -49,7 +49,7 @@ def make_broken_powerlaw_from_mcmc(table, e_ref):
     return naima.models.BrokenPowerLaw(
         amplitude=_get_amplitude_from_mcmc(table),
         e_0=e_ref,
-        e_break=_get_energy_from_log10(table, "log10_e_break_TeV"),
+        e_break=_get_energy_from_log10(table, "log10(e_break)"),
         alpha_1=_get_mcmc_median(table, "index_1"),
         alpha_2=_get_mcmc_median(table, "index_2"),
     )
@@ -61,7 +61,7 @@ def make_exponentialcutoff_powerlaw_from_mcmc(table, e_ref):
         amplitude=_get_amplitude_from_mcmc(table),
         e_0=e_ref,
         alpha=_get_mcmc_median(table, "index"),
-        e_cutoff=_get_energy_from_log10(table, "log10_e_cutoff_TeV"),
+        e_cutoff=_get_energy_from_log10(table, "log10(e_cutoff)"),
     )
 
 
@@ -97,7 +97,7 @@ def make_broken_powerlaw_ep_from_mcmc(
     model_e = naima.models.BrokenPowerLaw(
         amplitude=amp_e,
         e_0=e_ref_e,
-        e_break=_get_energy_from_log10(table, "log10_e_break_e_TeV"),
+        e_break=_get_energy_from_log10(table, "log10(e_break_e)"),
         alpha_1=_get_mcmc_median(table, "index_1_e"),
         alpha_2=_get_mcmc_median(table, "index_2_e"),
     )
@@ -105,7 +105,7 @@ def make_broken_powerlaw_ep_from_mcmc(
     model_p = naima.models.BrokenPowerLaw(
         amplitude=amp_e * ap_by_ae,
         e_0=e_ref_p,
-        e_break=_get_energy_from_log10(table, "log10_e_break_p_TeV)"),
+        e_break=_get_energy_from_log10(table, "log10(e_break_p)"),
         alpha_1=_get_mcmc_median(table, "index_1_p"),
         alpha_2=_get_mcmc_median(table, "index_2_p"),
     )
