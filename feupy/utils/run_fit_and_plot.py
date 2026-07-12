344a6e6af72f38ad94b8439cf480b5a4634ae9e9
Rubens Costa Jr. <rubensjrcosta@gmail.com>
2026-07-12 17:47:20 -0300

    fix bug show_plot add show_result_fit

--- feupy/utils/run_fit_and_plot.py
+++ feupy/utils/run_fit_and_plot.py
@@ -15,32 +15,31 @@ from feupy.utils.aic import calculate_aic
 __all__ = ["run_fit_and_plot"]
 
 
-def run_fit_and_plot(datasets, model, fitter=None, show_plot=True, **kwargs):
+def run_fit_and_plot(datasets, model, fitter=None, show_plot=True, show_result_fit=True, **kwargs):
 
     datasets_in = datasets.copy()
     datasets.models = Models([model])
 
     fitter = fitter or Fit()
     result_fit = fitter.run(datasets=datasets)
+    
+    if show_result_fit:
+        print(result_fit)
+        print(f'AIC: {calculate_aic(datasets, result_fit)}')
 
-    print(result_fit)
-    calculate_aic(datasets, result_fit)
+    if show_plot:    
+        ref_markers = build_fp_kwargs(datasets_in.names, marker_size=4)
+    
+        sed_plotter = SEDPlotter(
+            datasets=datasets_in,
+            models=Models(model.copy(name=f"FIT {model.spectral_model.tag[1]}")),
+        )
+    
+        plot_kwargs = {"ref_markers": ref_markers}
+        plot_kwargs.update(kwargs)
+    
+        fig = sed_plotter.plot(**plot_kwargs)
 
-    ref_markers = build_fp_kwargs(datasets_in.names, marker_size=4)
-
-    sed_plotter = SEDPlotter(
-        datasets=datasets_in,
-        models=Models(model.copy(name=f"FIT {model.spectral_model.tag[1]}")),
-    )
-
-    plot_kwargs = {"ref_markers": ref_markers}
-    plot_kwargs.update(kwargs)
-
-    fig = sed_plotter.plot(**plot_kwargs)
-
-    if show_plot:
         plt.show()
-    else:
-        plt.close(fig)
 
     return result_fit
